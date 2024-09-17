94dbb43fcf5b194bff7c7194be44311e38301f86
LiviuRimbu <145881286+LiviuRimbu@users.noreply.github.com>
2024-09-17 10:41:05 +0300

    Mesajul tău pentru commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
     return (
         <div className="flex h-screen justify-center items-center bg-red-500">
-            <h1 className="text-4xl font-bold text-blue-500">PWA</h1>
+            <h1 className="text-4xl font-bold text-blue-500">PWA APP</h1>
         </div>
     );
 }
