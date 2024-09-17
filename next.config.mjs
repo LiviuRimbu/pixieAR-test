2ab783723edf80d56287fda2a8ef5d995aa3a654
LiviuRimbu <145881286+LiviuRimbu@users.noreply.github.com>
2024-09-17 10:15:05 +0300

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
+import withPWA from 'next-pwa';
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: false,  // Enable service worker for testing in development mode
+})(nextConfig);
+
