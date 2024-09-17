f04126995bfc3ef4fb4f536245f7661a56de4dd4
LiviuRimbu <145881286+LiviuRimbu@users.noreply.github.com>
2024-09-17 15:34:30 +0300

    Adjust images

--- src/app/components/start-menu.tsx
+++ src/app/components/start-menu.tsx
@@ -9,17 +9,17 @@ const StartMenu: React.FC<StartMenuProps> = ({ handleImageClick }) => {
         <div className="flex flex-col items-center">
             <h1 className="text-3xl font-bold p-5">Welcome</h1>
             <p>Choose a book to scan</p>
-            <div className="flex p-10 gap-4 flex-row justify-between items-center">
+            <div className="flex p-10 gap-4 flex-row  items-center">
                 <img
                     src="/images/english.jpg"
                     alt="english book"
-                    className="w-1/3 h-[300px] cursor-pointer rounded-lg"
+                    className="w-1/2 h-1/2 cursor-pointer rounded-lg object-cover"
                     onClick={() => handleImageClick('https://mywebar.com/p/Project_0_qu59hfweju1802')}
                 />
                 <img
                     src="/images/grammar.jpg"
                     alt="grammar book"
-                    className="w-1/3 h-[300px] cursor-pointer rounded-lg"
+                    className="w-1/2 h-1/2 cursor-pointer rounded-lg object-cover"
                     onClick={() => handleImageClick('https://mywebar.com/p/Project_4_6k8la19ohc7569')}
                 />
             </div>
