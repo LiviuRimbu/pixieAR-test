1a5ac5b96a46e1545043dc85f401edecb346fe61
LiviuRimbu <145881286+LiviuRimbu@users.noreply.github.com>
2024-09-30 23:04:16 +0300

    Layout correction

--- src/app/components/lang-switcher.tsx
+++ src/app/components/lang-switcher.tsx
@@ -9,14 +9,15 @@ export default function LangSwitcher() {
     const pathWithoutLocale = pathname.replace(/^\/(en|ro)/, '');
 
     return (
-        <div className="absolute sm:mt-[3rem] right-3 sm:-right-9 -top-7 flex flex-row space-x-4 items-center text-violet text-[12px]">
-            <Link href={`/ro${pathWithoutLocale}`} locale="ro" className=" flex flex-col ">
-                <FlagRo className="w-5 h-5 rounded-[5px]"/>
-                <span className='text-center  text-violet font-black font-herculanum'>RO</span>
-            </Link>
+        <div className="absolute sm:mt-[3rem] right-3 sm:-right-9 -top-7 flex flex-row space-x-4 items-center text-violet text-[12px] z-10">
+
             <Link href={`/en${pathWithoutLocale}`} locale="en" className=" flex flex-col ">
                 <FlagUK className=" w-5 h-5 rounded-[5px]"/>
                 <span className='text-center  text-violet font-black font-herculanum'>EN</span>
+            </Link>
+            <Link href={`/ro${pathWithoutLocale}`} locale="ro" className=" flex flex-col ">
+                <FlagRo className="w-5 h-5 rounded-[5px]"/>
+                <span className='text-center  text-violet font-black font-herculanum'>RO</span>
             </Link>
         </div>
     );
