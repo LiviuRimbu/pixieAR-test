f40b501ba207b2148cd94eba65dc4834af9253b6
LiviuRimbu <145881286+LiviuRimbu@users.noreply.github.com>
2024-11-22 00:28:59 +0200

    UI modifications bkg image 1

--- src/app/[locale]/book-detail/[id]/page.tsx
+++ src/app/[locale]/book-detail/[id]/page.tsx
@@ -17,9 +17,10 @@ const BookDetail: React.FC = () => {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen"
 			 style={{
-				 backgroundImage: `linear-gradient(rgba(132, 229, 222, 0.4), rgba(148, 204, 253, 0.4)), url(${books[Number(id)].imageSrc})`,
-				 backgroundSize: 'cover',
-				 backgroundPosition: 'center',
+				 backgroundImage: `linear-gradient(rgba(132, 229, 222, 0.8), rgba(148, 204, 253, 0.9)), url(${books[Number(id)].imageSrc})`,
+				 backgroundSize: '120%',
+				 backgroundRepeat: 'no-repeat',
+				 backgroundPosition: 'center 80%',
 			 }}
 		>
 			<InfoHeader btnText = {t('button')} imageSrc={books[Number(id)].imageSrc} imgText={t('selected-image')} />
